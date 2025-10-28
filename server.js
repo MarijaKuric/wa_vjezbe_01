d0272ffa9a067138bf02250522b31776f191b2d9
MarijaKuric <159777612+MarijaKuric@users.noreply.github.com>
2025-10-28 11:22:31 +0100

    treci commit

--- server.js
+++ server.js
@@ -4,17 +4,14 @@ const path = require("path");
 const app = express();
 const PORT = 3000;
 
-// Ruta za početnu stranicu
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-// Ruta za "O nama"
 app.get("/about", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "about.html"));
 });
 
-// Ruta za korisnike (JSON)
 app.get("/users", (req, res) => {
   const users = [
     { id: 1, ime: "Ana", prezime: "Anić" },
@@ -24,7 +21,6 @@ app.get("/users", (req, res) => {
   res.json(users);
 });
 
-// Pokretanje poslužitelja
 app.listen(PORT, () => {
   console.log(`Server pokrenut na http://localhost:${PORT}`);
 });
